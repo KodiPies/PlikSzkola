1d2cd7e0731084a6f8fd64199e7e1f398b5eeee2
Tymek <114359697+KodiPies@users.noreply.github.com>
2025-09-10 12:14:49 +0200

    Update sortowanieSelection.cpp

--- cpp/sortowanie/sortowanie.cpp
+++ cpp/sortowanie/sortowanie.cpp
@@ -1,49 +1,58 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-void wypelnij(int tablicaDoWypełnienia[], int liczbaElementowTablicy)
-{
-    for (int i = 0; i < liczbaElementowTablicy; i++)
-    {
-        srand((i + 1) * time(NULL));
-        tablicaDoWypełnienia[i] = rand() % 11;
-    }
-}
-void wypisz(int tablicaDoWypisania[], int liczbaElementowTablicy)
-{
-    for (int i = 0; i < liczbaElementowTablicy; i++)
-    {
-        cout << tablicaDoWypisania[i] << " ";
-    }
-}
-int najmniejszyElement(int tablicaPrzeszukiwana[], int liczbaElementowTablicy)
-{
-    int minimalny = tablicaPrzeszukiwana[0];
-    int indexMinimalnego = 0;
-    for (int i = 0; i < liczbaElementowTablicy; i++)
-    {
-        if (tablicaPrzeszukiwana[i] < minimalny)
-        {
-            minimalny = tablicaPrzeszukiwana[i];
-            indexMinimalnego = i;
+
+// funkcja sortująca tablicę przez wybieranie
+void selectionSort(int tab[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i; // zakładamy, że i-ty element jest najmniejszy
+
+        // szukamy najmniejszego elementu w pozostałej części tablicy
+        for (int j = i + 1; j < n; j++) {
+            if (tab[j] < tab[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // zamiana miejscami i-tego elementu z najmniejszym
+        if (minIndex != i) {
+            int temp = tab[i];
+            tab[i] = tab[minIndex];
+            tab[minIndex] = temp;
         }
     }
-    for (int i = indexMinimalnego; i < liczbaElementowTablicy; i++)
-    {
-        tablicaPrzeszukiwana[i] = tablicaPrzeszukiwana[i + 1];
-    }
-    return minimalny;
 }
-int main()
-{
-    int tablicaTestowa[5], tablicaPosortowana[5];
-    wypelnij(tablicaTestowa, 5);
-    wypisz(tablicaTestowa, 5);
-    cout << endl;
-    for (int i = 5; i > -1; i--)
-    {
-        int min = najmniejszyElement(tablicaTestowa, i);
-        tablicaPosortowana[5 - i] = min;
+
+// wypełnienie tablicy losowymi liczbami
+void wypelnij(int tab[], int n) {
+    srand(time(NULL)); // losowanie tylko raz
+    for (int i = 0; i < n; i++) {
+        tab[i] = rand() % 11; // liczby 0–10
     }
-    wypisz(tablicaPosortowana, 5);
+}
+
+// wypisanie tablicy
+void wypisz(int tab[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int tab[5];
+    int n = 5;
+
+    wypelnij(tab, n);
+
+    cout << "Przed sortowaniem: ";
+    wypisz(tab, n);
+
+    selectionSort(tab, n);
+
+    cout << "Po sortowaniu: ";
+    wypisz(tab, n);
+
     return 0;
 }
